ad54f1342a2568f0c15021fe9d890e71c5d0ab6e
annv0r0 <annvv0r0@gmail.com>
2025-10-31 14:44:41 +0500

    S3 image storage

--- actions/upload.js
+++ actions/upload.js
@@ -5,11 +5,14 @@ import { parseCSV } from '@/lib/server/csv/parse';
 // import { insertItems } from '@/lib/server/db/MongoDB/items';
 import { insertItems } from '@/lib/server/db/SQL/items';
 
-export async function upload(key) {
-  const stream = await getObjectStream(key);
+export async function upload(content) {
+  //   bucketCSV: { kind: f.kind, name: f.filename, key, url }
+  //  bucketIMG: [{ kind: f.kind, name: f.filename, key, url }]
+  const { bucketCSV, bucketIMGlist } = content;
+  const stream = await getObjectStream(bucketCSV.key);
 
   try {
-    const docs = await parseCSV(stream);
+    const docs = await parseCSV(stream, bucketIMGlist);
 
     await insertItems(docs);
     return { ok: true };
--- components/Upload/UploadFileForm.js
+++ components/Upload/UploadFileForm.js
@@ -10,13 +10,12 @@ export default function UploadFileForm() {
 
   async function onSubmit(e) {
     e.preventDefault();
-    const file = e.target.elements.file.files[0];
-    // const image = e.target.files[0];
-    if (!file) return setError('Select a file first');
+    const csv = e.target.elements.file.files[0];
+    const images = e.target.elements.image.files;
+    if (!csv || !images) return setError('Select a file first');
 
-    const { key } = await uploadFileToS3(file);
-
-    const res = await upload(key);
+    const bucketContent = await uploadFileToS3(csv, images);
+    const res = await upload(bucketContent);
 
     if (!res.ok) {
       setError(res.error);
@@ -30,15 +29,22 @@ export default function UploadFileForm() {
   return (
     <>
       <form className={s.form} onSubmit={onSubmit}>
-        {/* <input type="file" name="image" id="image" accept="image/*"></input>
-      <label htmlFor="image">Choose image</label> */}
+        <div className={s.form__files}>
+          <div className={s.form__file}>
+            <input className={s.form__imageInput} type="file" name="image" id="image" accept="image/*" multiple></input>
+            <label className={s.form__imageLabel} htmlFor="image">
+              Choose image
+            </label>
+          </div>
 
-        <input className={s.form__fileInput} type="file" name="file" id="file"></input>
-        <label className={s.form__fileLabel} htmlFor="file">
-          Choose csv file
-        </label>
+          <div className={s.form__file}>
+            <input className={s.form__fileInput} type="file" name="file" id="file"></input>
+            <label className={s.form__fileLabel} htmlFor="file">
+              Choose csv file
+            </label>
+          </div>
+        </div>
 
-        <input className={s.form__textInput} type="text" name="text" placeholder="comments"></input>
         <button className={s.form__btn} type="submit">
           Send
         </button>
