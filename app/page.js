f9e7cd670b37043fb83ee0064d5e40e5af96369c
annv0r0 <annvv0r0@gmail.com>
2025-11-04 12:33:42 +0500

    ui: Item cards refactoring

--- app/page.js
+++ app/page.js
@@ -2,11 +2,12 @@ import s from './page.module.scss';
 import Image from 'next/image';
 
 export default async function Home() {
+  const S3_IMG_URL = `${process.env.NEXT_PUBLIC_S3_BUCKET}/images/hero.png`;
   return (
     <main className={s.main}>
       <h1 className={s.title}>SUPER-DUPER FULLSTACK-APP</h1>
       <div className={s.imageWrapper}>
-        <Image className={s.hero} src="https://cdn.thesimpsonsapi.com/500/episode/1.webp" alt="hero" fill></Image>
+        <Image className={s.hero} src={S3_IMG_URL} alt="hero" fill></Image>
       </div>
       <p className={s.blurb}>
         Track meals and spending in seconds. Upload receipts or CSVs, we parse them into items and expenses, store
