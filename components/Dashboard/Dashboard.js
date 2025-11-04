4f72475f2574bcdbe9aae479e263b6a5e1ae3638
annv0r0 <annvv0r0@gmail.com>
2025-11-04 11:45:20 +0500

    ui fixes

--- components/Dashboard/Dashboard.js
+++ components/Dashboard/Dashboard.js
@@ -1,15 +1,14 @@
+import { Suspense } from 'react';
 import ItemList from '@/components/Items/ItemList';
 import s from './Dashboard.module.scss';
-// import { getItems } from '@/lib/server/db/MongoDB/items';
-import { getItems } from '@/lib/server/db/SQL/items';
 
 export default async function Dashboard() {
-  const items = await getItems();
-
   return (
     <div className={s.container}>
       <h1 className={s.title}>YOUR ITEMS</h1>
-      <ItemList items={items} />
+      <Suspense fallback={<p className={s.loading}>Loading items...</p>}>
+        <ItemList />
+      </Suspense>
     </div>
   );
 }
